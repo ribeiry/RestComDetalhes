72d691706b3fc72e157ff13a250a7f1f70960614
Jose Augusto Ribeiro <ribeiry10@hotmail.com>
2017-03-01 22:28:08 -0300

    Rest da API com fotos e detalhes

---
+++ RestComXamarin/RestComXamarin/RestComXamarin/Models/Repository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace RestComXamarin.Models
+{
+    public class Repository
+    {
+        public async Task<List<Cat>> GetCats()
+        {
+            List<Cat> Cats;
+
+            var URLWebApi = "http://demos.ticapacitacion.com/cats";
+            using (var client = new System.Net.Http.HttpClient())
+            {
+                var JSON = await client.GetStringAsync(URLWebApi);
+                Cats = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Cat>>(JSON);
+            }
+            return Cats;
+
+        }
+    }
+}
